d15c4512953d505f0c1b1272ddc1959e9e2a6041
Mippy <mippysacc@gmail.com>
2025-04-10 20:23:02 -0400

    quick update

--- LIVETIMING/python/livetiming.py
+++ LIVETIMING/python/livetiming.py
@@ -1,14 +1,16 @@
 from srt_acquisition import SRT_ACQUISITION_DLL
 from xc_timer import XC_TIMER_DLL
 from ctypes import *
-from helpers import get_storage_location
 
 xc_instance = XC_TIMER_DLL()
-xc_instance.dll_initialize_dll_task(0x110, get_storage_location())
-xc_instance.dll_set_comm_port(2)
+xc_instance.dll_initialize_dll_task(0x110, "")
+xc_instance.dll_set_comm_port(0)
 xc_instance.dll_set_diagnostic_flags(1)
 xc_instance.dll_set_string_delimiter(0)
 xc_instance.dll_generate_dummy_record()
-record = xc_instance.dll_get_next_timer_record()
-for key,item in record.items():
-    print(f"Key: {key}  - Value: {item}")
+xc_instance.dll_synch_timers(0,"")
+while True:
+    record = xc_instance.dll_get_next_timer_record()
+    if record:
+        for key,item in record.items():
+            print(f"Key: {key}  - Value: {item}")
